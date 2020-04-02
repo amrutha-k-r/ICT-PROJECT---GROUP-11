654396aa918d835ba11587b966b8d794d38e3176
AMRUTHA K R <kramrutha88@gmail.com>
2020-04-02 14:12:27 +1000

    Update signup.php

--- signup.php
+++ signup.php
@@ -36,7 +36,9 @@
 
 <html>
 <head>
- 
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Turnos</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
