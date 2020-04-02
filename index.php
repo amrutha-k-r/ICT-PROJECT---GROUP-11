1821123d1527860f159408e975cb1961fed8c094
AMRUTHA K R <kramrutha88@gmail.com>
2020-04-02 13:48:45 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("location: welcome.php");
+  exit;
+}
+ 
+// Include config file
+require_once "./db/config.php";
+ 
+if(isset($_POST['log']))
+    {
+        $eid=$_POST['eid'];
+        $pass=$_POST['pass'];
+        
+        $qry="select * from employee where empid='$eid'";
+        
+        $result = mysqli_query($link, $qry);
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+//            echo "here";
+            
+            while($row = mysqli_fetch_assoc($result)) {
+                
+                echo "db".$row['empid']."in".$eid;
+                
+//                if(strcmp($row['empid'], $eid)==0 )
+//                {
+                    if(password_verify($pass, $row['password']))
+                    {
+                        echo "login success'";
+                        $_SESSION['id']=$row['id'];
+                        $_SESSION['branch']=$row['branch'];
+                        $_SESSION['name']=$row['name'];
+                        
+                        if($row['type']==1)
+                        {
+                            header("location:admin.php");
+                        }
+                        else if($row['type']==0)
+                        {
+                            
+                            header("location:home.php");
+                        }
+                        else if($row['type']==10)
+                        {
+                            
+                            header("location:sup.php");
+                        }
+                        else
+                        {
+                            echo '<script>alert("Contact Your Manager for Login");</script>';
+                            echo '<script>window.location="index.php";</script>';
+                        }
+                        
+                    }
+                    else
+                    {
+                        echo "<script>alert('Password Mismatch');</script>";
+                        
+                    }
+                    
+//                }
+//                else
+//                { 
+////                    echo "<br".$row['password'];
+//                    echo "<script>alert('Login Failed.. Invalid Details');</script>";
+//                    break;
+//                }
+            }
+        } else {
+            echo "<script>alert('Login Failed.. Invalid Details');</script>";
+        }
+    
+        
+    
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v3.11.1, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v3.11.1, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--<link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">-->
+  <meta name="description" content="">
+  <title>Turnose</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+  <link rel="stylesheet" href="assets/bootstrap-material-design-font/css/material.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/animate.css/animate.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+    <section class="mbr-section" id="msg-box5-3" style="background-color: rgb(193, 138, 32); padding-top: 160px; padding-bottom: 0px;">
+
+    
+    <div class="container">
+        <div class="row">
+            <div class="mbr-table-md-up">
+
+              
+
+              <div class="mbr-table-cell col-md-5 text-xs-center text-md-right content-size">
+                  <h5 class="mbr-section-title display-2"><br>TURNOSE</h5>
+                  <div class="lead">
+
+                    <form action="" method="post">
+                      
+                      <input type="number" id="login" value="" class="fadeIn second" name="eid" placeholder="Employee ID" required>
+                      <input type="password" id="password" value="" class="fadeIn third" name="pass" placeholder="Password" required>
+                      <br> <input type="submit" class="fadeIn fourth" id="logb" value="Log In" name="log">
+                      
+                      
+                    
+                   </form>
+
+                  </div>
+
+                  <div> 
+<!--                      <a class="fadeIn fourth" href="signup.php">
+                          <input type="button" value="Sign Up"></a>-->
+<a class="underlineHover fadeIn fourth" href="signup.php">New User?</a>
+                  </div>
+              </div>
+
+
+              
+
+
+              <div class="mbr-table-cell mbr-left-padding-md-up mbr-valign-top col-md-7 image-size" style="width: 50%;">
+                  <div class="mbr-figure"><img src="assets/images/desktop.jpg"></div>
+              </div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+  <!--<section class="engine"><a rel="external" href="https://mobirise.com">https://mobirise.com/</a></section><script src="assets/web/assets/jquery/jquery.min.js"></script>-->
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/SmoothScroll.js"></script>
+  <script src="assets/viewportChecker/jquery.viewportchecker.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+  <input name="animation" type="hidden">
+  </body>
+</html>
