42b6607d3962cd9e5ca6d4741c78bb47708e8141
Jane <59048325+JaneTom21@users.noreply.github.com>
2020-04-03 12:44:18 +1000

    Update addrooster.php

--- addrooster.php
+++ addrooster.php
@@ -44,6 +44,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
   <!--<link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">-->
   
   <title>Turnos</title>
