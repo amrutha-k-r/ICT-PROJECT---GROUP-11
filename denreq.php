e11a69013ec7e83532ac89f6adbdf808125d7a1d
JaneTom <59048325+JaneTom21@users.noreply.github.com>
2020-04-24 04:24:16 +1000

    Update denreq.php

--- denreq.php
+++ denreq.php
@@ -102,7 +102,7 @@
                             <?php
                                 }
                             ?>
-                        <!--<li class="nav-item"><a class="nav-link link" href="">Leave Requests</a></li>-->
+
                         <li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="" aria-expanded="false">Leave</a>
                                 <div class="dropdown-menu">
                                     <a class="dropdown-item" href="leavereq.php">Requests</a>
