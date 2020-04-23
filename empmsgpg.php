9061a2af4aa06919744564639be34680085f1c2b
JaneTom <59048325+JaneTom21@users.noreply.github.com>
2020-04-24 04:24:24 +1000

    Update empmsgpg.php

--- empmsgpg.php
+++ empmsgpg.php
@@ -10,13 +10,13 @@
 ?>
 <html>
 <head>
-  <!-- Site made with Mobirise Website Builder v3.11.1, https://mobirise.com -->
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="generator" content="Mobirise v3.11.1, mobirise.com">
+
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!--<link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">-->
-  <meta name="description" content="Website Maker Description">
+
   <title>Turnos</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
@@ -91,8 +91,7 @@
                         <li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="" aria-expanded="false">LEAVE</a>
                                 <div class="dropdown-menu">
                                     <a class="dropdown-item" href="addreq.php">Add Requests</a>
-<!--                                    <a class="dropdown-item" href="denreq.php">Denied</a>
-                                    <a class="dropdown-item" href="sanreq.php">Sanctioned</a>-->
+
                                     <a class="dropdown-item" href="leavehis.php">History</a>
                                 </div>
                         </li>
@@ -125,8 +124,7 @@
       <div class="container">
           <div class="row">
               <div class="col-xs-12 text-xs-center">
-<!--                  <h3 class="mbr-section-title display-2">PRICING TABLE</h3>
-                  <small class="mbr-section-subtitle">Pricing table with four columns and solid color background.</small>-->
+
 
                 
 
