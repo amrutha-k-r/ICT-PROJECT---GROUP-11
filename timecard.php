4602d6ad785727203f878de4b9e922060cf14cd7
AMRUTHA K R <kramrutha88@gmail.com>
2020-05-22 00:31:40 +1000

    Update timecard.php

--- timecard.php
+++ timecard.php
@@ -75,6 +75,7 @@
                     <div class="navbar-brand">
                         
                         <a class="navbar-caption text-black" href="">Turnos</a>
+                        <li class="nav-item nav-btn"><h5 style="color: #FF9900;">Welcome <?php echo $_SESSION['name']; ?>..</h5></li>
                     </div>
 
                 </div>
@@ -212,6 +213,7 @@
   <script src="assets/touchSwipe/jquery.touchSwipe.min.js"></script>
   <script src="assets/viewportChecker/jquery.viewportchecker.js"></script>
   <script src="assets/theme/js/script.js"></script>
+  <script src='https://kit.fontawesome.com/a076d05399.js'></script>
   
   
   <input name="animation" type="hidden">
